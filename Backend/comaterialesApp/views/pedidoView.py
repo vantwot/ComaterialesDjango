d7e9db772dd23b9002a27baf1933c40cd1bcbf6e
Valentina <svaalentina123@gmail.com>
2021-10-24 17:59:57 -0500

    views

--- Backend/comaterialesApp/views/pedidoView.py
+++ Backend/comaterialesApp/views/pedidoView.py
@@ -2,8 +2,11 @@ from rest_framework import status, views, generics
 from rest_framework.response import Response
 from comaterialesApp.serializers.pedidoSerializer import PedidoSerializer
 from comaterialesApp.models.pedido import Pedido
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework.permissions import IsAuthenticated
 
 class pedidos(views.APIView):
+    autenticacion = [authentication.TokenAuthentication]
     def get(self, request, format=None): 
         
         return Response(serializer.data)
